66d5a3197da9f15d3d1e3fd32b46f5e0f7e3de42
Bryant Lee <cloud9bryant@gmail.com>
2024-02-16 15:00:06 -0800

    4941 - Working version of Cognito implementation. Going to proceed to MQTT Greencore.

---
+++ client/src/services/AuthService.js
@@ -0,0 +1,54 @@
+import {CognitoUserPool, AuthenticationDetails, CognitoUser, CognitoUserAttribute} from 'amazon-cognito-identity-js';
+
+const poolData = {
+  UserPoolId: 'us-west-2_PgylBvXXN',
+  ClientId: '3mo3jpnuqo30jsp1l7vekj52fl' // Your Cognito App Client ID
+};
+
+const userPool = new CognitoUserPool(poolData);
+
+export const signUp = (username, password, email, onSuccess, onFailure) => {
+  const attributeList = [
+    new CognitoUserAttribute({
+      Name: 'email',
+      Value: email
+    })
+  ];
+
+  userPool.signUp(username, password, attributeList, null, (err, result) => {
+    if (err) {
+      onFailure(err);
+      return;
+    }
+    onSuccess(result);
+  });
+};
+
+export const signIn = (username, password, onSuccess, onFailure, onNewPasswordRequired) => {
+  const authenticationDetails = new AuthenticationDetails({
+    Username: username,
+    Password: password,
+  });
+
+  const cognitoUser = new CognitoUser({
+    Username: username,
+    Pool: userPool
+  });
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: onSuccess,
+    onFailure: onFailure,
+    newPasswordRequired: (userAttributes, requiredAttributes) => {
+      console.log('New password is required');
+      delete userAttributes.email_verified; // Delete attributes you don't want to update
+      onNewPasswordRequired(userAttributes, requiredAttributes);
+    }
+  });
+};
+
+export const signOut = () => {
+  const cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser) {
+    cognitoUser.signOut();
+  }
+};

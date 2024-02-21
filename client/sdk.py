795c10139f460d326440f558d7fe2d92f9daf059
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-02-20 18:41:47 -0800

    Add moquitto_pub line

--- client/sdk.py
+++ client/sdk.py
@@ -4,7 +4,10 @@ import os
 # Initialize the MQTT Client
 myMQTTClient = AWSIoTMQTTClient("MyRaspberryPiClient")
 myMQTTClient.configureEndpoint("a1h7gui22xor7w-ats.iot.us-west-2.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("/home/kelly/Downloads/AmazonRootCA1(1).pem", "/home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-private.pem.key", "/home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-certificate.pem.crt")
+myMQTTClient.configureCredentials(
+    "/home/kelly/Downloads/AmazonRootCA1(1).pem", 
+    "/home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-private.pem.key", 
+    "/home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-certificate.pem.crt")
 
 # Attempt to connect to AWS IoT
 try:
@@ -20,3 +23,5 @@ try:
     print("Message published successfully.")
 except Exception as e:
     print(f"Failed to publish message: {e}")
+
+# mosquitto_pub -h a1h7gui22xor7w-ats.iot.us-west-2.amazonaws.com -p 8883 -t "test/topic" -m "Hello, MQTT client!" --cafile /home/kelly/Downloads/AmazonRootCA1(1).pem --cert /home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-certificate.pem.crt --key /home/kelly/Downloads/e5531071a0ee9ef7df45278ad334ac909f47364a9346671c2e3c806460d6489c-certificate.pem.crt -q 1
